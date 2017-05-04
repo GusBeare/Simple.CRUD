4f222db59686b8f4ee18e139e7b3be3bd35c292c
Gus Beare <gus@carawaydesign.com>
2017-05-04 22:20:07 +0100

    Add files via upload

---
+++ Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Nancy.Bootstrapper;
+using Nancy.Gzip;
+using Nancy.TinyIoc;
+
+namespace BayLeafWebSite
+{
+    using Nancy;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            // Enable Compression with Default Settings
+            pipelines.EnableGzipCompression();
+
+            base.ApplicationStartup(container, pipelines);
+        }
+    }
+}
