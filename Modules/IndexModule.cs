9ad7f05e184d532f4a84785285c4ed96037d0d38
Gus Beare <gus@carawaydesign.com>
2017-05-08 16:25:38 +0100

    more refinements to the concept

--- Modules/IndexModule.cs
+++ Modules/IndexModule.cs
@@ -15,18 +15,42 @@ namespace SimpleCRUD
                 return View["index"];
             };
 
+            // load contact form
             Get["/contact"] = p =>
             {
-                ViewBag.Method = "Insert";
+                ViewBag.Method = "insert";
                 return View["contact"];
             };
 
+
+
+            // read products
             Get["/products"] = p =>
             {
-                ViewBag.Method = "List";
+                ViewBag.Method = "read";
+                return View["product-list"];
+            };
+
+            // insert a product
+            Get["/products/insert"] = p =>
+            {
+                ViewBag.Method = "create";
                 return View["products"];
             };
 
+            // update a product
+            Get["/products/update"] = p =>
+            {
+                ViewBag.Method = "update";
+                return View["products"];
+            };
+
+            // delete a product
+            Get["/products/insert"] = p =>
+            {
+                ViewBag.Method = "update";
+                return View["products"];
+            };
 
         }
     }
